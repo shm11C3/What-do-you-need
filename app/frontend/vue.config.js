d690cf3656926a41f7b6b12828268d2e8b19e961
shm <s.murakami.tech@gmail.com>
2022-07-06 17:13:56 +0000

    Update: `devServer` をhttps対応

--- app/frontend/vue.config.js
+++ app/frontend/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    https: true,
+    host: "0.0.0.0",
+    port: "8083",
+    allowedHosts: "strn.home",
+  },
 });
