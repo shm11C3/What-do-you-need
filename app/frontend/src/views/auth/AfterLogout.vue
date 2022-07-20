2531c0dd2ef021a59881f031345fcb1b4c3b3433
shm <s.murakami.tech@gmail.com>
2022-07-20 15:21:20 +0000

    Update: ローディング時のスピナーを作成

--- app/frontend/src/views/auth/AfterLogout.vue
+++ app/frontend/src/views/auth/AfterLogout.vue
@@ -1,9 +1,17 @@
+<template>
+  <LoadingSpinner class="mt-20" />
+</template>
 <script>
 import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import LoadingSpinner from "../../components/parts/LoadingSpinner.vue";
 
 export default {
+  name: "AfterLogout",
+  components: {
+    LoadingSpinner,
+  },
   setup() {
     const router = useRouter();
     const store = useStore();
