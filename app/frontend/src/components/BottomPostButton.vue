2c51bd9cee394109ad721bc8e4be06806dfe9091
shm <s.murakami.tech@gmail.com>
2022-09-27 04:43:53 +0000

    Add: Fixed button to new post page

---
+++ app/frontend/src/components/BottomPostButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fixed bottom-10 lg:bottom-12 right-5 lg:right-12">
+    <router-link
+      to="/post/create"
+      v-show="pathToDisplayButtons.includes(path)"
+      class="bg-blue-500 hover:bg-blue-700 text-white text-lg font-bold py-4 px-8 rounded-full shadow-lg"
+      >Write Post</router-link
+    >
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const pathToDisplayButtons = ["/"];
+
+const path = computed(() => {
+  return route.path;
+});
+</script>
