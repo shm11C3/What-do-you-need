8c8ee470ac4d2a124faf356a0997a8ea3ed0697f
shm <s.murakami.tech@gmail.com>
2022-12-07 14:51:02 +0000

    Add: reaction send function

---
+++ app/src/js/consts/reactionType.js
@@ -0,0 +1,25 @@
+export default function () {
+  const reaction_types = {
+    good: "👍",
+    "more detail": "🧐",
+    "smiling face with heart-eyes": "🥰",
+    "raised hand": "✋",
+    congratulations: "🎉",
+    "unique idea": "🚀",
+    hot: "🔥",
+    perfect: "💯",
+    "red heart": "❤",
+    "orange heart": "🧡",
+    "yellow heart": "💛",
+    "green heart": "💚",
+    "blue heart": "💙",
+    "purple heart": "💜",
+    "brown heart": "🤎",
+    "black heart": "🖤",
+    "white heart": "🤍",
+  };
+
+  return {
+    reaction_types,
+  };
+}
